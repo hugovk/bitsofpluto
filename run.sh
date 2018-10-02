66e52a37e041f625f2fa55c468e4f0958e493d07
Hugo <hugovk@users.noreply.github.com>
2018-10-02 13:06:32 +0300

    Update YAML path

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-python3 bitsofpluto.py --yaml data/bitsofpluto_sample.yaml --pluto data/crop_p_color2_enhanced_release.7000x7000.png
+python3 bitsofpluto.py --yaml ~/bin/data/bitsofpluto.yaml --pluto data/crop_p_color2_enhanced_release.7000x7000.png
