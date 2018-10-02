e768488bc7b297ab6c4d5d04ac0bc8cc59617626
Hugo <hugovk@users.noreply.github.com>
2018-10-02 11:11:58 +0300

    Remove -x

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-python3 bitsofpluto.py --yaml data/bitsofpluto_sample.yaml --pluto data/crop_p_color2_enhanced_release.7000x7000.png -x
+python3 bitsofpluto.py --yaml data/bitsofpluto_sample.yaml --pluto data/crop_p_color2_enhanced_release.7000x7000.png
