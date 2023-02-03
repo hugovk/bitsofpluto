bf1c730eb4bbdfd1cf7602eeeadf15d79d09f739
Hugo van Kemenade <hugovk@users.noreply.github.com>
2023-02-03 22:32:01 +0200

    Use default image path

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-python3 bitsofpluto.py --yaml ~/bin/data/bitsofpluto.yaml --pluto data/crop_p_color2_enhanced_release.7000x7000.png --no-web
+python3 bitsofpluto.py --yaml ~/bin/data/bitsofpluto.yaml --no-web
